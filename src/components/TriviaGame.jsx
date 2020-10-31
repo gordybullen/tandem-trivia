022e1a86a0ca73ed9746dda72ca8360fa2d038ca
Gordy <gordybullen@gmail.com>
2020-10-30 21:11:53 -0700

    initial commit

---
+++ src/components/TriviaGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TriviaGame() {
+  return (
+    <div className="game-container">
+      Welcome to Tandem Trivia!!!
+    </div>
+  )
+}
+
+export default TriviaGame
