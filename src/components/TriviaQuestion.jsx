d812ed0dbc6f559033a0f1beb89da1b1ae312080
Gordy <gordybullen@gmail.com>
2020-10-31 21:23:24 -0700

    edit answer reveal display

--- src/components/TriviaQuestion.jsx
+++ src/components/TriviaQuestion.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import "../styles/TriviaQuestion.css";
 
-const TriviaQuestion = ({ questionObj, submitSelected }) => {
+const TriviaQuestion = ({
+  questionObj: { question, incorrect, correct },
+  submitSelected,
+}) => {
   const [selected, setSelected] = useState("");
-  const options = questionObj.incorrect.concat(questionObj.correct);
-  // if no option is selected, disable the answer submit button
+  const [submitted, setSubmitted] = useState(false);
+
+  // combine the array of incorrect answers and the correct answer to get all
+  // the options
+  const options = incorrect.concat(correct);
+
+  // if no option is selected, disable the answer submit button using a boolean
   const submitDisabled = Boolean(!selected);
 
   const handleSelect = (e) => {
@@ -18,28 +26,51 @@ const TriviaQuestion = ({ questionObj, submitSelected }) => {
   };
 
   const handleSubmit = () => {
+    setSelected("");
+    setSubmitted(false);
     submitSelected(selected);
   };
-console.log(submitDisabled)
+
+  const optionSelect = () => {
+    return (
+      <>
+        <h2 className="title">{question}</h2>
+        <div className="optionsContainer">
+          {options.map((option, idx) => {
+            return (
+              <div
+                className={option === selected ? "option selected" : "option"}
+                key={`option-${idx}`}
+                onClick={handleSelect}
+              >
+                {option}
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={() => setSubmitted(true)} disabled={submitDisabled}>
+          Submit answer
+        </button>
+      </>
+    );
+  };
+
+  const answerReveal = () => {
+    return (
+      <>
+        <div>Correct answer: {correct}</div>
+        <div>Your answer: {selected}</div>
+        <div>
+          {selected === correct ? "Great job!!!" : "You'll get it next time!"}
+        </div>
+        <button onClick={handleSubmit}>Next question!</button>
+      </>
+    );
+  };
+
   return (
     <div className="questionContainer">
-      <h2 className="title">{questionObj.question}</h2>
-      <div className="optionsContainer">
-        {options.map((option, idx) => {
-          return (
-            <div
-              className={option === selected ? "option selected" : "option"}
-              key={`option-${idx}`}
-              onClick={handleSelect}
-            >
-              {option}
-            </div>
-          );
-        })}
-      </div>
-      <button onClick={handleSubmit} disabled={submitDisabled}>
-        Submit answer
-      </button>
+      {!submitted ? optionSelect() : answerReveal()}
     </div>
   );
 };
